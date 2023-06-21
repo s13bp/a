5da97d701b9b392a7a2862195d1bbe93bb7cb400
s13bp <sahibbharajparminder@gmail.com>
2023-06-21 05:18:37 +0300

    puta

--- server.js
+++ server.js
@@ -37,7 +37,13 @@ if(err) throw err;
 console.log('Connected to MongoDB')
 });
 
-
+if (process.env.NODE_ENV === "production") {
+    console.log("PRODUCTION MODE ACTIVE");
+    //app.use('/', express.static("/client/build"));
+    app.use(express.static("./client/build"));
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, './client/build','index.html'));
+    })}
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
